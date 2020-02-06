d406937d4fc6cf6fe83b15da7567fcc89e2bb067
menzimkhize <43467504@mylife.unisa.ac.za>
2020-02-06 21:32:56 +0200

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -10,13 +10,28 @@ var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection', (socket) => {
+io.on('connection',  (socket) => {
     console.log('New user connected');
+    
+    socket.emit('newEmail', {
+        from: 'mike@example.com',
+        text: 'Hey. What is going on.',
+        createdAt: 123
+    });
+
+    socket.on('createEmail', (newEmail) => {
+        console.log('createEmail', newEmail);
+    });
+
+    socket.on('disconnect', function() {
+        console.log('User was disconnected');
+    });  
+
   });
 
-io.on('disconnect', () => {
-    console.log('User was disconnected');
-  });
+
+
+
 app.use(express.static(publicPath));
 
 server.listen(port,() => {
